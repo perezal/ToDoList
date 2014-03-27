ed3037887258319bf69ebddb2e1384b9669a8144
Alex Perez <alexperez@Alexs-MacBook-Air.local>
2014-03-27 12:00:10 -0700

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Alex Perez on 3/27/14.
+//  Copyright (c) 2014 Alex Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
